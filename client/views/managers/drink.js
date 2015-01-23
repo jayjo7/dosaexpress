acc6203009e785501d50c23ce22f32c0d40f72cb
jayjo7 <jayjo7.hotmail.com>
2015-01-22 23:00:58 -0800

    Menu is reactive

---
+++ client/views/managers/drink.js
@@ -0,0 +1,30 @@
+Template.drink.helpers({
+
+	breakFast: function()
+	{
+		//return Foods.find({$and : [{Activate: "Y"}, {Category: "Appetizer"}]},{sort: { WebId: 1 } });
+		return Menu.find({Category: "Drink"});
+	}
+	,
+
+    currency: function(num)
+    {
+        return '$' + Number(num).toFixed(2);
+    },
+
+    isAvailable: function(Availability)
+    {
+    	if(Availability==='SoldOut')
+    		return false;
+    	else
+    		return true;
+    },
+
+    soldOut:function(Availability)
+    {
+    	if(Availability==='SoldOut')
+    		return 'soldout';
+    	else
+    		return '';
+    },
+});
